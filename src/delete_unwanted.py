c8f15f8b65806a6b7eadd4df000a4e4a2531f0da
ChaimShvadron <s0527649958@gmail.com>
2025-09-01 22:22:38 +0300

    Add UnwantedDeleter class to delete non-antisemitic documents with neutral or positive sentiment and no weapons

---
+++ src/delete_unwanted.py
@@ -0,0 +1,29 @@
+from elasticsearch import Elasticsearch
+
+class UnwantedDeleter:
+    def __init__(self, es_client: Elasticsearch, index_name: str):
+        self.es = es_client
+        self.index_name = index_name
+    
+    def delete_unwanted(self):
+        """Delete documents that are not antisemitic, have no weapons, and sentiment is neutral or positive."""
+        query = {
+            "query": {
+                "bool": {
+                    "must": [
+                        {"term": {"Antisemitic": False}},
+                        {"terms": {"sentiment": ["neutral", "positive"]}},
+                        {
+                            "script": {
+                                "source": "doc['weapons'].size() == 0",
+                                "lang": "painless"
+                            }
+                        }
+                    ]
+                }
+            }
+        }
+        
+        response = self.es.delete_by_query(index=self.index_name, body=query)
+        print(f"Deleted {response['deleted']} unwanted documents.")
+        return response
